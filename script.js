080cfb495051c0396664b42b4d6b688f6870940b
edisamsuar <madinatuddiniyahbabussalam@gmail.com>
2025-06-14 06:16:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         'Admin': { password: 'admin123', link: 'https://docs.google.com/forms/d/e/1FAIpQLSeJNCQj8UjEZVelI0TSb4r-6np_NE1iDujK9aJ0KaAJJnUUeA/viewform' },
         'hafizatunnafisah': { password: 'hfzatunn0321', link: 'https://edisamsuar.github.io/aksesadminhfz' },
         'nurulalfalah': { password: 'nrlalfalah3561', link: 'https://edisamsuar.github.io/aksesadminnrl' },
-        'user4': { password: 'pass4', link: 'https://link4.com' },
+        'ujian': { password: 'bbs', link: 'https://edisamsuar.github.io/ujianlokalbbs' },
         'user5': { password: 'pass5', link: 'https://link5.com' }
     };
 
